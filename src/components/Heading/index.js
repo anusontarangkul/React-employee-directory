f259acfcc1d3ee8d290c7fcba24d718b6529b6f9
David Anusontarangkul <anusontarangkul.d@gmail.com>
2020-11-14 10:28:43 -0800

    add Heading

--- src/components/Heading/index.js
+++ src/components/Heading/index.js
@@ -2,7 +2,12 @@ import React from 'react';
 import "./style.css";
 
 function Heading() {
-    return (null)
+    return (
+        <header className="header-style">
+            <h1 className="h1-style">Employee Directory</h1>
+            <p className="p-style">Find Your Team Members Here!</p>
+        </header>
+    )
 }
 
 export default Heading;
---
+++ src/components/Search/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import "./style.css";
+
+function Search() {
+    return (null)
+}
+
+export default Search;
