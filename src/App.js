fad49f76d21d67178dce077607fe71bfb49c385b
David Anusontarangkul <anusontarangkul.d@gmail.com>
2020-11-14 10:39:08 -0800

    add search

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ function App() {
     <div className="App">
       <Wrapper>
         <Heading />
+        <Search />
       </Wrapper>
     </div>
   );
--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import "./style.css";
 
-function Search() {
-    return (null)
+function Search(props) {
+    return (
+        <form className="search">
+            <div className="form-group">
+                <input
+                    value={props.search}
+                    onChange={props.handleInputChange}
+                    name="term"
+                    list="term"
+                    type="text"
+                    className="form-control"
+                    placeholder="Search"
+                    id="term"
+                />
+            </div>
+        </form>
+    )
 }
 
 export default Search;
