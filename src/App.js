2f64c6e70b34106fcc7490d95d4c093d6fc8a014
David Anusontarangkul <anusontarangkul.d@gmail.com>
2020-11-14 17:33:27 -0800

    change heading to table div

--- src/App.js
+++ src/App.js
@@ -14,25 +14,27 @@ import './App.css';
 
 function App() {
 
-  function handleChange(searched) {
-    const matchingEmployee = [];
-    for (let i = 0; i < employeeList.length; i++) {
-      let currentEmployee = employeeList[i];
-      if (currentEmployee.name.inclues(searched)) {
-        matchingEmployee.push(currentEmployee)
-      }
-    }
-    setEmployes({
-      type: "change",
-      input: matchingEmployee
-    });
-  }
+  // function handleChange(searched) {
+  //   const matchingEmployee = [];
+  //   for (let i = 0; i < employeeList.length; i++) {
+  //     let currentEmployee = employeeList[i];
+  //     if (currentEmployee.name.inclues(searched)) {
+  //       matchingEmployee.push(currentEmployee)
+  //     }
+  //   }
+  //   setEmployes({
+  //     type: "change",
+  //     input: matchingEmployee
+  //   });
+  // }
   return (
     <div className="App">
       <Wrapper>
         <Heading />
-        <Search handleChange={handleChange} />
-        <Employees employeeList={employeeList} matchingEmployee={matchingEmployee} />
+        {/* <Search handleChange={handleChange} /> */}
+        <Search />
+        {/* <Employees employeeList={employeeList} matchingEmployee={matchingEmployee} /> */}
+        <Employees employeeList={employeeList} />
       </Wrapper>
     </div>
   );
--- src/components/Employees/index.js
+++ src/components/Employees/index.js
@@ -2,65 +2,85 @@ import React from 'react';
 import "./style.css";
 
 
-function Employees(props) {
+function Employees({ employeeList }) {
     return (
-        <div className="container container-style">
-            <div className="row row-style container-style">
-                <div className="col col-title">
-                    <p>Image</p>
-                    {props.employeeList.map(employee => (
-                        <img src={employee.image} className="list-group-item"
-
-                        />
-                    ))}
-                </div>
-                <div className="col col-title">
-                    <p>Name</p>
-                    {props.employeeList.map(employee => (
-                        <li className="list-group-item">
-                            {employee.name}
-                        </li>
-
-
-                    ))}
-
-                </div>
-                <div className="col col-title">
-                    <p>Phone</p>
-                    {props.employeeList.map(employee => (
-                        <li className="list-group-item">
-                            {employee.phone}
-                        </li>
-
-
-                    ))}
-
-
-                </div>
-                <div className="col col-title">
-                    <p>Email</p>
-                    {props.employeeList.map(employee => (
-                        <li className="list-group-item">
-                            {employee.email}
-                        </li>
-
-
-                    ))}
-
-                </div>
-                <div className="col col-title">
-                    <p>DOB</p>
-                    {props.employeeList.map(employee => (
-                        <li className="list-group-item">
-                            {employee.DOB}
-                        </li>
-                    ))}
-
-                </div>
-
-            </div>
-
+        <div className="container">
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th scope="col">Image</th>
+                        <th scope="col">Name</th>
+                        <th scope="col">Phone</th>
+                        <th scope="col">Email</th>
+                        <th scope="col">DOB</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {employeeList.map(employee => <tr key={employee.id}>
+                        <td><img id={employeeList.name} src={employeeList.image} /></td>
+                        <td>{employeeList.name}</td>
+                        <td>{employeeList.phone}</td>
+                        <td>{employeeList.email}</td>
+                        <td>{employeeList.DOB}</td>
+                    </tr>)}
+                </tbody>
+            </table>
         </div>
+
+
+
+
+
+
+
+
+
+        // <div className="container container-style">
+        //     <div className="row row-style container-style">
+        //         <div className="col col-title">
+        //             <p>Image</p>
+        //             {props.employeeList.map(employee => (
+        //                 <img src={employee.image} className="list-group-item"
+        //                 />
+        //             ))}
+        //         </div>
+        //         <div className="col col-title">
+        //             <p>Name</p>
+        //             {props.employeeList.map(employee => (
+        //                 <li className="list-group-item">
+        //                     {employee.name}
+        //                 </li>
+        //             ))}
+        //         </div>
+        //         <div className="col col-title">
+        //             <p>Phone</p>
+        //             {props.employeeList.map(employee => (
+        //                 <li className="list-group-item">
+        //                     {employee.phone}
+        //                 </li>
+        //             ))}
+        //         </div>
+        //         <div className="col col-title">
+        //             <p>Email</p>
+        //             {props.employeeList.map(employee => (
+        //                 <li className="list-group-item">
+        //                     {employee.email}
+        //                 </li>
+        //             ))}
+        //         </div>
+        //         <div className="col col-title">
+        //             <p>DOB</p>
+        //             {props.employeeList.map(employee => (
+        //                 <li className="list-group-item">
+        //                     {employee.DOB}
+        //                 </li>
+        //             ))}
+        //         </div>
+        //     </div>
+        //     <div className="employee-body">
+
+        //     </div>
+        // </div>
     )
 }
 
